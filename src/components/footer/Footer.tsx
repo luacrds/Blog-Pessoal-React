3384a963428f2fbcf242130c3da9bf1bb518e46e
unknown <luanacardosos@outlook.com>
2025-05-25 17:17:03 -0300

    feat  Página de Login - Estilização

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-4">
-      <p>&copy; 2025 - Blog Pessoal</p>
-    </footer>
-  );
+
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Generation | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/in/luanacardosodasilva7/" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                          </a>
+                          <a href="https://www.instagram.com/seu_usuario" target="_blank">
+                            <InstagramLogo size={48} weight='bold' />
+                          </a>
+                          <a href="https://www.facebook.com/seu_usuario" target="_blank">
+                            <FacebookLogo size={48} weight='bold' />
+	                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Footer;
+export default Footer
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
-  return (
-    <nav className="bg-gray-900 text-white px-6 py-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold hover:text-gray-300 transition-colors">
-          Blog Pessoal
-        </Link>
+    return (
+        <>
+            <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
+                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-        <div className="space-x-4">
-          <Link to="/home" className="hover:text-gray-300 transition-colors">
-            Home
-          </Link>
-          <Link to="/cadastro" className="hover:text-gray-300 transition-colors">
-            Cadastro
-          </Link>
-        </div>
-      </div>
-    </nav>
-  );
+                <div className='flex gap-4'>
+                    Postagens
+                    Temas
+                    Cadastrar tema
+                    Perfil
+                    Sair
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Navbar;
+export default Navbar
