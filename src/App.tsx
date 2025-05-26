7b83069f7ecce357f8126d227f031b4cc99d59da
unknown <luanacardosos@outlook.com>
2025-05-26 14:58:21 -0300

    feat adiciona context

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+
+import Cadastro from './pages/cadastro/Cadastro';
+import { AuthProvider } from './context/UserContext';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+
 
 function App() {
+
   return (
-    <>
-        <BrowserRouter>
-          <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
